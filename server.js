875b80048d254e0f16c9c2157f6def24ea02d5b8
Eliel das Virgens Santos <eliel.floyd@bol.com.br>
2019-02-14 22:27:07 -0300

    Update server.js
    algumas pequenas correções

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
-//Install express server
 const express = require('express');
 const app = express();
 const path = require('path');
-const httpServer = require('http').createServer(app)
-const io = require('socket.io')(httpServer);
 const port = process.env.PORT || 8080;
 
-// Serve only the static files form the dist directory
 app.use(express.static('./'));
 
-httpServer.listen(port,function() {
+app.listen(port,function() {
     console.log('run http server '+port);
 })
